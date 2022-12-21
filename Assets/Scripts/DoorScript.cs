3d34d572c3ccc025201bf032d6c5586f6569c8b1
Dan8Oren <dan.js.oren@gmail.com>
2022-12-21 21:51:01 +0200

    -Door Animation and Scene change animation

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorScript : MonoBehaviour
+{
+    public Sprite openDoorSprite;
+    public Animator transition;
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private string nextScene; 
+    [SerializeField] private float transitionWaitTime;
+    [SerializeField] private float waitTimeBeforeTransition;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // If the collider that entered the trigger has the "Player" tag, animate the door and load the new scene
+        if (col.CompareTag("Player"))
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            AnimateDoor(openDoorSprite);
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    // Plays the Door open animation
+    private void AnimateDoor(Sprite sprite)
+    {
+        _spriteRenderer.sprite = sprite;
+    }
+
+    // Loads the specified scene after a short delay
+    private IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(waitTimeBeforeTransition);
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionWaitTime);
+        SceneManager.LoadSceneAsync(nextScene);
+    }
+}
