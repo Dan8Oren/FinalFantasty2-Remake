3e5de2c8c76a49fc3d80d05ddb24b887477877a0
Dan8Oren <dan.js.oren@gmail.com>
2023-03-01 18:14:58 +0200

    code reformatting

--- Assets/Scripts/ScriptableObjectsScripts/RegularAttackData.cs
+++ Assets/Scripts/ScriptableObjectsScripts/RegularAttackData.cs
@@ -6,7 +6,7 @@ public class MeleeAttackData : ScriptableObject
     public string displayName;
     public int sequence;
     public string info;
-    public int damage;
+    public int pointsOfEffect;
 
     [Tooltip("if the attack effects all of the enemy group")]
     public bool effectAllEnemyGroup;
